0f1bad584c6650cfa6fcbc740c5715e4b4b65604
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-04-05 17:08:49 +0200

    created project with create-react-app and added files for homepage

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HomePage.styles.scss';
+
+export const HomePage = () => {
+    return(
+        <div className = 'homepage'>
+            <div className= 'directory-menu'>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            Шось там 
+                        </h1>
+                        <span className="subtitle">І ше шось</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
